24373ce663b93c203614418965d889213048eb3b
dgalan <dgalan@geprom.com>
2022-09-21 22:44:22 +0200

    Actualizado

--- dist/urls.js
+++ dist/urls.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.APIUrlEquipos = exports.APIUrlMatriculas = exports.APIUrlGrupos = exports.APIUrlAlumnos = exports.APIUrlProfesores = exports.host = void 0;
 //export const host = 'http://147.83.118.92';
 exports.host = "http://localhost";
 exports.APIUrlProfesores = exports.host + ":3000/api/Profesores";
